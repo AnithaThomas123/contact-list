5e34a0bca1f81ae128394328cef79ec5e400f07f
AnithaThomas123 <anithathomas452@gmail.com>
2023-02-08 17:34:15 +0530

    New updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, ContactsComponent],
+  declarations: [AppComponent, NavigationComponent, ContactsComponent, FirstComponent],
   imports: [BrowserModule, AppRoutingModule, AccordionModule],
   providers: [],
   bootstrap: [AppComponent],
